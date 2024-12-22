a39c1d66a5dcddf71770588017d23f8e76e1fd49
rusik <ruslanmovsesan@gmail.com>
2024-12-22 15:17:01 +0900

    mainProject

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
 // @todo: Темплейт карточки
+function templateCard(element) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    cardElement.querySelector('.card__image').src = element.link;
+    cardElement.querySelector('.card__image').alt = element.name;
+    cardElement.querySelector('.card__title').textContent = element.name;
+    deleteButton.addEventListener('click', () => removeCard(cardElement));
+    console.log(cardElement);
+    return cardElement;
+
+}
 
 // @todo: DOM узлы
+const placeList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-
+function addCard(cards) {
+    cards.forEach((card) => {
+        const cardElement = templateCard(card,  removeCard);
+        placeList.appendChild(cardElement);
+    })
+}
 // @todo: Функция удаления карточки
+function removeCard(cardElement) {
+    cardElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
+addCard(initialCards)
